e3cde5d497307c5c4449bfc1cb5c0af65f30591f
UNDERBUFFER <ruslanekitsky@gmail.com>
2020-08-01 17:36:19 +0300

    work with keys

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+'use strict';
 const MAP_X = 560
 const MAP_Y = 320
 const FIGURE_X = 40
@@ -7,16 +8,55 @@ const FIGURE_Y = 40
 const div = document.getElementById('block1')
 let snake = []
 
-randomInteger = (min, max) => {
+const randomInteger = (min, max) => {
     const rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
 }
 
+Array.prototype.rotate = function() {
+    let res = this.slice(1)
+    res.push(...this.slice(0, 1))
+    return res
+}
+
 // получение стартовой позиции:
-const x = randomInteger(0, (MAP_X / FIGURE_X) - 1) * 40
-const y = randomInteger(0, (MAP_Y / FIGURE_Y) - 1) * 40
+let x = randomInteger(0, (MAP_X / FIGURE_X) - 1) * 40
+let y = randomInteger(0, (MAP_Y / FIGURE_Y) - 1) * 40
 
 snake.push([x, y])
+
+document.addEventListener('keydown', (event) => {
+    // определение зажатой клавиши:
+    switch (event.key) {
+        case 'ArrowUp':
+            if ((y - FIGURE_Y) < 0)
+                y = MAP_Y - FIGURE_Y
+            else
+                y -= FIGURE_Y
+            break;
+        case 'ArrowDown':
+            if ((y + FIGURE_Y) >= MAP_Y)
+                y = 0
+            else
+                y += FIGURE_Y
+            break;
+        case 'ArrowLeft':
+            if ((x - FIGURE_X) < 0)
+                x = MAP_Y - FIGURE_X
+            else
+                x -= FIGURE_X
+            break;
+        case 'ArrowRight':
+            if ((x + FIGURE_X) >= MAP_X)
+                x = 0
+            else
+                x += FIGURE_X
+            break;
+    }
+    snake = snake.rotate()
+    snake[0] = [x, y]
+    console.log(snake[0])
+})
 
 for (let block of snake) {
     const innerDiv = document.createElement('div')
